d97f5e6b315b077a832ad57dee503766bc04c3db
Ajaybabu <ajaybabupenumalli@gmail.com>
2024-05-17 12:49:38 +0530

    path is changed

--- curd/urls.py
+++ curd/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path, include
-from.views import post,put,patch
+from.views import post,put,patch,ajay
 
 urlpatterns = [
     path('post/',post),
     path('put/',put),
-    path('patch/',patch)
+    path('patch/',patch),
+    path('ajay/',ajay),
 ] 
